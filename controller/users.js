aaacf12613ae105f517785ded4b501d3ef26e5d6
bidhan2058 <jonson.dhital123.bd@gamil.com>
2022-04-16 15:16:59 +0545

    profile of user get and edit feature

--- controller/users.js
+++ controller/users.js
@@ -1,17 +1,55 @@
 // const db = require('../database');
 // const jwt =require ('jsonwebtoken');
 // const bcrypt = require('bcryptjs');
+const UserModel = require('../models/user.model');
+const userQuery = require('../helpers/user.query');
 
-// exports.updateuser= async(req,res) =>{
-// try{
-//         const {name,password, passwordconfirm, phone} = req.body;
-//     db.query('SELECT email from user WHERE email = ?',[email], async(error,results) => { //we are suing async due to password await code
-//          if(error){
-//                 console.log(error);
-//          }
-//     })
-// }
-// catch(error){
-//     console.log(error);
-// }
-// }
+exports.getUser = (req,res,next)=>{
+    const id = req.params.id;
+    console.log("id",id)
+    UserModel.find({_id : id},(err,users)=>{
+        if(err){
+            return next({
+                msg : err,
+                status : 400
+            })
+        }
+        if(users){
+            res.json({
+                users
+            })
+        }
+    })
+}
+
+exports.updateuser=(req,res,next) =>{
+    const id = req.params.userID;
+    console.log("userId inside update user",id)
+    const data = req.body;
+    console.log("u[date profile",data);
+    UserModel.findOne({_id : id}, (err,user)=>{
+        if(err){
+            return next({
+                msg : err,
+                status : 400
+            })
+        }
+        if(user){
+             let userinfo = userQuery.mapUser(data,user);
+             userinfo.save((err,done)=>{
+                if(err){
+                    return next({
+                        msg : err,
+                        status : 400
+                    })
+                }
+                if(done){
+                    res.json({
+                        success: true,
+                        user : done
+                    })
+                }
+            })
+        }
+    })
+}
