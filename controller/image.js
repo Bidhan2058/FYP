5bd081bfadef36513d9e5d869df9f4c2251032b5
bidhan2058 <jonson.dhital123.bd@gamil.com>
2022-04-19 09:30:07 +0545

    image and imageroute

--- controller/image.js
+++ controller/image.js
@@ -3,31 +3,34 @@ const path  = require('path');
 const { getVehicleById } = require('../services/vehicle');
 
 function singleImg (req,res,next) {
-    var fileInfo = req.file;
-    console.log(fileInfo);
-    var vehicleID = req.params.vehicleID;
-    console.log("vehicel"+vehicleID)
+    var fileInfo = req.file;  //file ko name yeha aaucha multer bata success vayesi 
+    var vehicleID = req.params.vehicleID;  //parameterko id liyeko
     if (req.file.filename) {
         console.log("File"+fileInfo)
-        getVehicleById(vehicleID)
-        .then((vehicle)=>{
-            vehicle.img = 'http://10.0.2.2:8000/static/' + fileInfo.filename;
+        getVehicleById(vehicleID)  // promise ko repsonse aayesi matrta .then call huncha 
+        .then((vehicle)=>{ //success huda / resolve huda data yeha aaucha 
+            vehicle.vehicleImg = 'http://10.0.2.2:8000/static/' + fileInfo.filename;
+            // IF WE PUT LOCALHOST IN THE URL ,THEN FLUTER GIVES US ERROR SO WE ASRE USING 10.0.2.2
             vehicle.save((err,done)=>{
                 if(err){
                     return next({
                         msg: "Image couldnot be saved",
                         status : 400
                     })
+                    // {
+                    //     msg: "Image couldnot be saved",
+                    //     status : 400
+                    // }
                 }
                 else{
-                    res.json({
+                    res.json({  // RES.JSON VNAYA FRONTEND MA DATA PATHAUNI
                         msg : "Image added successfully",
                         status : 200 
                     })
                 }
             })
         })
-        .catch((error)=>{
+        .catch((error)=>{ //reject huda /error huda
             return next({
                 msg : err,
                 status : 400
@@ -42,10 +45,10 @@ function singleImg (req,res,next) {
     }
 }
 
-function getImageID(req, res) {
-    res.sendFile('C:/Users/brother/OneDrive/Desktop/FYP/Backend/Frontuploads' + req.params.id);
-}
+// function getImageID(req, res) {
+//     res.sendFile('C:/Users/brother/OneDrive/Desktop/FYP/Backend/Frontuploads' + req.params.id);
+// }
 module.exports ={
     singleupload : singleImg,
-    getImageID,
+
 };
--- routes/images/imagesroute.js
+++ routes/images/imagesroute.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const  TokenController  = require('../../controller/token');
 const imageController = require('../../controller/image');
-const imageUploader = require('../../helpers/imageuploader');
+const imageUploader = require('../../helpers/imageuploader'); //multer 
 
 const router = express.Router();
 
 router.post('/singleupload/:vehicleID', imageUploader.imgupload.single('image'),imageController.singleupload);
+// getting vehicleID paramertetr from fotnend
+router.put('/uploadEdit/:vehicleID', imageUploader.imgupload.single('image'),imageController.singleupload);
 
 // router.get('/getimg/:id',imageController.getImageID)
 
